45a605e7ebc49e7f5ea7fed9c85e1b830ebfeb05
GedasPupa <gedosas@gmail.com>
2021-07-01 06:46:09 +0300

    autogonkes.js  -  justGo()

--- autogonkes.js
+++ autogonkes.js
@@ -24,8 +24,12 @@ class Car {
         this.distance -= this.speed;
     }
 
+    justGo() {
+        this.distance += this.speed;
+    }
+
     hundredKm() {
-        if (this.distance >= hundred) {
+        if (this.distance >= hundred && !finish) {
             console.log(`Nuvaziuota ${hundred}km. Laimi: ${this.name}`);
             hundred += 100;
         }
@@ -97,6 +101,7 @@ function lenktynes(cars) {
                 cars[i].hundredKm();
                 cars[i].isWinner();
             } else {
+                cars[i].justGo();
                 cars[i].hundredKm();
                 cars[i].isWinner();
             }
@@ -124,5 +129,46 @@ generuok();
 lenktynes(cars);
 rusiuok();
 
-// console.log(cars);
+console.log(cars);
 console.log(cars[0]);
+
+/*
+UŽDUOTIS:
+
+yra 8 masinos
+
+pavadinimas
+greitis: 0
+kelias: 0
+
+masinos lenktyniauja (ciklas)
+
+kiekvienos iteracijos metu:
+visos masinos gali pakeisti savo greiti
+    random skaicius
+    jei random < 0.2 - masina stabdo (t.y. mazeja masinos greitis 1..5 (random))
+    jei random < 0.7 - masina gazuoja (t.y. dideja masinos greitis 1..10 (random))
+    priesingu atveju masinos greitis nesikeicia
+visos masinos pavaziuoja: per tiek, koks yra jos greitis
+
+lenktynes baigiasi, kai bent viena masina nuvaziuoja 1000 km
+
+reikia surusiuoti masinas pagal nuvaziuota kelia,
+jei dvieju masinu kelias vienodas - pirmesne yra ta, kurios didesnis greitis
+
+atspausdinti turnyrine lentele
+
+* kas 100 km spausdinti lyderi
+
+PAPILDOMA UŽDUOTIS (KLASĖS):
+
+dvi masinos turi buti sportines
+
+sportine masina turi tureti spoileri (pradzioj nuleistas)
+
+jei spoileris yra pakeltas - sportine masina stabdo 2x greiciau (gazuoja standartiskai)
+jei spoileris yra nuleistas - sportine masina gazuoja 2x greiciau (stabdo standartiskai)
+
+lenktyniu metu kieviena sportine masina su 50% tikimybe pakeicia spoilerio pozicija
+
+*/
